8181524f84911829e0f16661e7223bee2f5b5013
Fast-toe <fausto2017@gmail.com>
2022-04-26 23:33:49 -0500

    getting user input added

--- Major Maker/Major Maker/Source.cpp
+++ Major Maker/Major Maker/Source.cpp
@@ -1,21 +1,63 @@
 #include <iostream> 
 using namespace std;
 
-void menu() {
+void majorSelection() 
+{
+
+	std::cout << "Insert Name and Major\n";
+	std::cout << "1.Welding Technology, Associate of Applied Science\n2.General Studies, Associate of Arts\n"
+		<< "3.Accounting, Associate of Applied Science\n4.Real Estate, Associate of Applied Science\n"
+		<< "5.Eye Care Technology, Associate of Applied Science";
+	
+}
+
+void menu() 
+{
 	int choice;
-	do
+
+	bool running = true;
+	while (running) //menu
 	{
-		cout << "0. Get Started\n1." << std::endl << "1. Quit\n2";
+		// 0 will start the program, 1 will end the program
+		cout << "0. Get Started\n" << "1. Quit\n";
 		int choice;
-		std::cin >> choice;
-		
+		std::cin >> choice; 
+
+		string userName; //store name and major selection
+		int majorNum;
+				
 		switch (choice)
 		{
 		case 0:
-			std::cout << "Insert Name and Major\n";
+			majorSelection();
+			cin >> userName; // get name and major
+			cin >> majorNum;
+			if (majorNum = 1)
+			{
+				cout << userName << " You have pick Welding Technology, Associate of Applied Science";
+			}
+			else if (majorNum = 2)
+			{
+				cout << userName << " You have pick General Studies, Associate of Arts";
+			}
+			else if (majorNum = 3)
+			{
+				cout << userName << " You have pick Accounting, Associate of Applied Science";
+			}
+			else if (majorNum = 4)
+			{
+				cout << userName << " You have pick Real Estate, Associate of Applied Science";
+			}
+			else
+			{
+				cout << userName << " You have pick Eye Care Technology, Associate of Applied Science";
+			}
+
+			running = false;
 			break;
 		case 1:
-			std::cout << "Thank You/n";
+			std::cout << "Thank You\n";
+			running = false;
 			break;
 		}
 	}
@@ -23,5 +65,5 @@ void menu() {
 
 int main()
 {
-	menu()
+	menu();
 }
